f4d969759fa60b2c3535919c93ec81cafe50e116
mahadi-nsu <mahadi.hassan@northsouth.edu>
2019-10-28 20:07:52 +0600

    location traked done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Map from "./components/Map";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Map hackathon</h1>
+       <Map />
     </div>
   );
 }
 
 export default App;
+
+//  const TOKEN = 'pk.eyJ1IjoibWFoYWRpLW5zdSIsImEiOiJjazI3Nmt1aWgwczNoM21reGgzZzhtNGJiIn0.vxdLJWEwFqL_w5nPmtIthg';
---
+++ src/components/Map.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'reactn';
+import ReactMapGL, { Marker } from "react-map-gl";
+import useLocation from "../hooks/UseLocation";
+
+const TOKEN =  "pk.eyJ1IjoibWFoYWRpLW5zdSIsImEiOiJjazI3Nmt1aWgwczNoM21reGgzZzhtNGJiIn0.vxdLJWEwFqL_w5nPmtIthg";
+
+const Map = () => {
+    const location = useLocation();
+    const [viewport, setViewport] = useState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: 31.9742044,
+      longitude: -49.25875,
+      zoom: 2,
+    });
+  
+    useEffect(() => {
+      const handleResize = () => {
+        setViewport({
+          ...viewport,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    });
+  
+    useEffect(() => {
+      if (location) {
+        setViewport((vp) => ({
+          ...vp,
+          ...location,
+          zoom: 8,
+        }));
+      }
+    }, [location, setViewport]);
+
+     const addData = () =>{
+       console.log("clicked");
+    }
+  
+    return (
+       
+        <ReactMapGL
+          mapStyle="mapbox://styles/mapbox/dark-v9"
+          mapboxApiAccessToken={TOKEN}
+          {...viewport}
+          onViewportChange={(vp) => setViewport(vp)}
+        >
+          {location ? (
+            <Marker
+              latitude={location.latitude}
+              longitude={location.longitude}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <span style={{ fontSize: `${viewport.zoom * 0.5}rem` }}
+               onClick = {addData}
+              >📸</span>
+            </Marker>
+          ) : null}
+        </ReactMapGL>
+    );
+  };
+  
+  export default Map;
